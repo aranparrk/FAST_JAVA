863bf56bff75e089a6cddfd9e266975d34f762ce
aranparrk <aranparrk@gmail.com>
2022-04-29 12:03:10 +0900

    11. 자바의 연산자 - 1 (대입, 부호, 산술, 복합대입, 증감연산자)

---
+++ chapter1/src/ch11/OperatorTest.java
@@ -0,0 +1,13 @@
+package ch11;
+
+public class OperatorTest {
+
+    public static void main(String[] args) {
+        int gameScore = 150;
+        int lastScore = gameScore++; // gameScore += 1; gameScore = gameScore + 1;
+
+        System.out.println(gameScore); // ++gameScore : 151, gameScore++ : 151
+        System.out.println(lastScore); // ++gameScore : 151, gameScore++ : 150
+    }
+
+}
