3dcb38b82e453dba56c35af8f4cbc734ed39d0e7
aranparrk <aranparrk@gmail.com>
2022-04-30 18:59:08 +0900

    02. 생활 속에서 객체를 찾아 클래스로 구현

---
+++ chapter2/src/ch01/Student.java
@@ -0,0 +1,11 @@
+package ch01;
+
+// public : 공용, 어디서든 접근 가능하다
+public class Student {
+    // 멤버변수
+    int studentNumber;
+    String studentName;
+    int majorCode;
+    String majorName;
+    int grade;
+}
