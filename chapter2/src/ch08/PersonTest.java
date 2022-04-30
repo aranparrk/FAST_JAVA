65b126d356f7430fc23d8ecd1a6108f228053151
aranparrk <aranparrk@gmail.com>
2022-04-30 20:37:00 +0900

    08. 복습 - 객체 구현하기

---
+++ chapter2/src/ch08/PersonTest.java
@@ -0,0 +1,13 @@
+package ch08;
+
+public class PersonTest {
+
+    public static void main(String[] args) {
+
+        Person person = new Person(180, 78, "남성", "Tomas", 37);
+
+        System.out.println(person.showPerson());
+
+    }
+
+}
