3ee98d166d089c25ed9023e3e4e2bce3d0672359
aranparrk <aranparrk@gmail.com>
2022-05-01 13:07:26 +0900

    08. 복습 - 객체 구현하기

--- chapter2/src/ch08/Person.java
+++ chapter2/src/ch08/Person.java
@@ -20,8 +20,4 @@ public class Person {
         return "키가 " + height + "이고 몸무게가 " + weight + "킬로인 " + gender + "이 있습니다. 이름은 " + name + "이고 나이는 " + age + "세 입니다.";
     }
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> origin/master
--- chapter2/src/ch08/PersonTest.java
+++ chapter2/src/ch08/PersonTest.java
@@ -10,8 +10,4 @@ public class PersonTest {
 
     }
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> origin/master
