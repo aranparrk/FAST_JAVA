9612b070bee8e1ba6e212f90d047761fa16a5ded
aranparrk <aranparrk@gmail.com>
2022-05-03 20:28:37 +0900

    19. 복습 - static과 싱글톤 패턴

---
+++ chapter2/src/ch19/Car.java
@@ -0,0 +1,16 @@
+package ch19;
+
+public class Car {
+
+    private static int serialNum = 1000;
+    private int carNum;
+
+    public Car() {
+        serialNum++;
+        carNum = serialNum;
+    }
+
+    public int getCarNum() {
+        return carNum;
+    }
+}
