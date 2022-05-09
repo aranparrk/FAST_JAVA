d633713f5a88d9b40e1ced3b15311482c3695eb3
aranparrk <aranparrk@gmail.com>
2022-05-09 23:24:37 +0900

    24. ArrayList를 활용 간단한 성적 산출 프로그램

---
+++ chapter2/src/ch24/Student.java
@@ -0,0 +1,35 @@
+package ch24;
+
+import java.util.ArrayList;
+
+public class Student {
+
+    private int studentID;
+    private String studentName;
+
+    ArrayList<Subject> subjectList;
+
+    public Student(int studentID, String studentName) {
+        this.studentName = studentName;
+        this.studentID = studentID;
+
+        subjectList = new ArrayList<>();
+    }
+
+    public void addSubject(String name, int point) {
+        Subject subject = new Subject();
+        subject.setName(name);
+        subject.setScorePoint(point);
+
+        subjectList.add(subject);
+    }
+
+    public void showScoreInfo() {
+        int total = 0;
+        for(Subject subject : subjectList) {
+            total += subject.getScorePoint();
+            System.out.println(studentName + "학생의 " + subject.getName() + "의 점수는 " + subject.getScorePoint() + "점 입니다.");
+        }
+        System.out.println(studentName + "학생의 총점은 " + total + "점 입니다.");
+    }
+}
