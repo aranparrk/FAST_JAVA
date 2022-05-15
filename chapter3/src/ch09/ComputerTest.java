0e794ccf374322689029be4ddc1d654711f4aa33
aranparrk <aranparrk@gmail.com>
2022-05-15 14:42:28 +0900

    09. 추상 클래스의 의미와 구현하는 방법

---
+++ chapter3/src/ch09/ComputerTest.java
@@ -0,0 +1,13 @@
+package ch09;
+
+public class ComputerTest {
+
+    public static void main(String[] args) {
+
+        // 추상클래스는 인스턴스화 할 수 없다
+        Computer desktop = new Desktop();
+        desktop.display();
+
+    }
+
+}
